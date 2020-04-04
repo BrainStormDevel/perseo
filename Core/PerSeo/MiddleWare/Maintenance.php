102a34117994871f4d6c2905037b33d98616873a
BrainStormDevel <pendley@libero.it>
2020-04-04 18:32:19 +0000

    Apply fixes from StyleCI

--- Core/PerSeo/MiddleWare/Maintenance.php
+++ Core/PerSeo/MiddleWare/Maintenance.php
@@ -21,14 +21,18 @@ class Maintenance
         $this->request = $request;
         $this->response = $response;
         if ($this->container->has('settings.global') && ($this->container->get('settings.global')['maintenance']) && ((!isset($_COOKIE['maintenance'])) || $_COOKIE['maintenance'] != $this->container->get('settings.global')['maintenancekey'])) {
-            $lang = new \PerSeo\Translator($this->container->get('current.language'),
-                \PerSeo\Path::LangPath('maintenance'));
+            $lang = new \PerSeo\Translator(
+                $this->container->get('current.language'),
+                \PerSeo\Path::LangPath('maintenance')
+            );
             $langall = $lang->get();
             $this->container->set('view', function ($container) {
-                $view = new \Slim\Views\Twig('modules/maintenance/views/'.$this->container->get('settings.global')['template'],
+                $view = new \Slim\Views\Twig(
+                    'modules/maintenance/views/'.$this->container->get('settings.global')['template'],
                     [
                         'cache' => 'cache',
-                    ]);
+                    ]
+                );
                 $router = $this->container->get('router');
                 $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
                 $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
